50297f90b5c73c7ddd254f79f0d3cee3ae03a8ec
melike elma <melikapple.80@gmail.com>
2021-01-08 00:23:02 +0300

    add todos components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 import './App.css'
+import TodoList from './todos/TodoList'
 
 const App = () => (
   <div className="root">
-    <h1>Hello</h1>
+    <TodoList />
   </div>
 )
+
 export default hot(module)(App)
