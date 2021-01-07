b0e38d1ff731dde1dc176074d509fbe21571ecc0
melike elma <melikapple.80@gmail.com>
2021-01-07 23:44:30 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+import './App.css'
+
+const App = () => (
+  <div className="root">
+    <h1>Hello</h1>
+  </div>
+)
+export default hot(module)(App)
